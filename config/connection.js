281dae0bc4d40b4f1acad8bdac9566ad703452af
Rupa Gadewar <rupa@gadewar.com>
2023-08-06 22:44:36 -0500

    Heroku configuration

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,24 @@
-//connection.js
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  }
-);
 
+// If running on Heroku, use JawsDB URL; otherwise, use local MySQL configuration
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {
+    dialect: 'mysql'
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 // Export the Sequelize instance
 module.exports = sequelize;
