026c908267a664df543dcfe7ccf15ca2b239acd9
Rupa Gadewar <rupa@gadewar.com>
2023-08-14 16:49:15 -0500

    CSS changes for login and sign up page

--- public/js/create-post.js
+++ public/js/create-post.js
@@ -1,23 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   // Your create-post.js code here
   const createPostHandler = async (event) => {
     event.preventDefault();
 
-    const title = document.querySelector('#post-title').value;
-    const content = document.querySelector('#content').value;
+    const title = document.querySelector("#post-title").value;
+    const content = document.querySelector("#content").value;
 
     if (title && content) {
-      const response = await fetch('/api/posts', {
-        method: 'POST',
+      const response = await fetch("/api/posts", {
+        method: "POST",
         body: JSON.stringify({ title, content }),
         headers: {
-          'Content-Type': 'application/json'
-        }
+          "Content-Type": "application/json",
+        },
       });
 
       if (response.ok) {
         // Post created successfully, reload the dashboard to see the updated list of posts
-        window.location.replace('/dashboard');
+        window.location.replace("/dashboard");
       } else {
         // Post creation failed, show the error message returned by the server
         const responseData = await response.json();
@@ -26,5 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   };
 
-  document.querySelector('#new-post-form').addEventListener('submit', createPostHandler);
+  document
+    .querySelector("#new-post-form")
+    .addEventListener("submit", createPostHandler);
 });
